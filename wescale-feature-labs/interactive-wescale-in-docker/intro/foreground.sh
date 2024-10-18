ed64a156efdbe5416c1400919b4502a4909e8edb
earayu <earayu@163.com>
2024-10-18 17:08:13 +0800

    update

--- wescale-feature-labs/interactive-wescale-in-docker/intro/foreground.sh
+++ wescale-feature-labs/interactive-wescale-in-docker/intro/foreground.sh
@@ -1,4 +1,4 @@
 until nc -z 127.0.0.1 15306; do
   echo "Waiting for wescale on port 15306 to be available..."; 
   sleep 5;
-done && sleep 5 && echo "Port 15306 is open."
+done && echo "Port 15306 is open." && docker exec -it wescale mysql -h127.0.0.1 -P15306
