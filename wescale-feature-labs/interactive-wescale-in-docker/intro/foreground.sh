db5bbfa829f296517e9d9fcf84274e798fc9d660
earayu <earayu@163.com>
2024-10-18 17:01:44 +0800

    update

--- wescale-feature-labs/interactive-wescale-in-docker/intro/foreground.sh
+++ wescale-feature-labs/interactive-wescale-in-docker/intro/foreground.sh
@@ -2,4 +2,4 @@ apt install mysql-client-core-8.0 && \
 until nc -z 127.0.0.1 15306; do
   echo "Waiting for wescale on port 15306 to be available..."; 
   sleep 5;
-done && echo "Port 15306 is open."
+done && sleep 5 && echo "Port 15306 is open."
